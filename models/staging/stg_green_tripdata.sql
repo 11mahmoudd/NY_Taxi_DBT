640b038bf34c62c9857a46ec3fab3fa406b9cf07
Mahmoud Mohamed <mahmoudahmedbedeir@gmail.com>
2025-03-10 16:18:08 +0000

    create new table and schema files

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -45,8 +45,8 @@ where rn = 1 and trip_type is not null
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
+-- {% if var('is_test_run', default=true) %}
 
-  limit 100
+--   limit 100
 
-{% endif %}
+-- {% endif %}
